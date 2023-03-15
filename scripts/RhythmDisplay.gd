e456edd51fe9f39740cc45605f10db3f2049acaa
Graves <theredhead242@gmail.com>
2023-03-15 14:11:48 -0700

    spinning rhythm display thing

--- scripts/RhythmDisplay.gd
+++ scripts/RhythmDisplay.gd
@@ -3,19 +3,25 @@ extends Node
 
 @export var marker: PackedScene
 
-@export var offset : float = 100
+@export var offset : float = 80
 
+
+#so markers all spawn at the same place
+#then they start rotating
+#so they actually need to get spawned as a child object still
+#however they need to get spawned roated back?
 func SpawnMarker(index: int, beat: float):
 	var markerInst = marker.instantiate()
 	#so the position should first be offset from the center by an amount
-	$RecordIcon/Markers.add_child(markerInst)
+#	$RecordIcon/CenterAnchor/Markers.add_child(markerInst)
+	$RecordIcon/CenterAnchor/Markers.add_child(markerInst)
 	#so the beat should get remaindered first
 	#so we map the beat on 8 beats to 2pi
 	var beatPos = fposmod(beat,8.0)/8.0
 	beatPos *= 2 *PI
 
-	var pos :Vector2 = $RecordIcon/CenterAnchor.position + Vector2.LEFT.rotated(beatPos) * offset * (index + 1)
-	markerInst.rotation = $RecordIcon/CenterAnchor.rotation
+	var pos :Vector2 = $RecordIcon/CenterAnchor.position + Vector2.LEFT.rotated(-beatPos)* offset * (3 -index + 1)
+	markerInst.rotation = -$RecordIcon/CenterAnchor.rotation
 
 	markerInst.position = pos
 	

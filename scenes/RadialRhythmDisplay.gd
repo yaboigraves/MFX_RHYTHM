4a24a3e7abd6ad72de2fe14870682f9ac4d334ad
Graves <theredhead242@gmail.com>
2023-03-18 17:17:46 -0700

    line renderers for beat divisions

--- scenes/RadialRhythmDisplay.gd
+++ scenes/RadialRhythmDisplay.gd
@@ -1,11 +1,9 @@
 class_name RadialRhythmDisplay
 extends Control
 
-#TODO: line render to create visuals for beat divisions
 #TODO: line render to create lanes dynamically
 #TODO: better control for marker placement 
 #TODO: specify valid arc area better than current setup
-
 
 
 @export var marker : PackedScene
@@ -25,11 +23,20 @@ var markers = []
 
 func _ready() -> void:
 	%Arm.visible = rhythmArmVisible
+	CreateBeatGridLines()
 	
 func _on_metronome_tick(timeSeconds, timeBeats) -> void:
 	%Arm.rotation =  ((timeBeats * PI)/(beatsPerRotation/2.0))
 
+func CreateBeatGridLines():
+	for i in range(beatsPerRotation):
+		var line = Line2D.new()
+		line.width = 2
+		line.add_point(Vector2())
+		line.add_point( Vector2(size.x/2.0,0).rotated( i * (PI/(beatsPerRotation/2.0))))
+		%LineRenderers.add_child(line)
 
+	
 #TODO: move to a seperate marker spawner thing, or not idk
 func SpawnMarker(index,beatPosition: float):
 		var markerInst = marker.instantiate()
